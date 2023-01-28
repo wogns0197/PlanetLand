3e30a8560f18a86fb661c4fa3ba63248ac8788d2
KwonJaehoon <wogns0197@gmail.com>
2023-01-28 18:27:11 +0900

    Fishing Rod

---
+++ Script/Fish.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private Rigidbody rg;
+    int randSpeed, randRotY, randRepeat;
+    bool bAggro;
+    float ExitAggroTime;
+    void Start()
+    {
+        bAggro = false;
+        ExitAggroTime = 0;
+
+        randRepeat = Random.Range(1, 10);
+        rg = this.GetComponent<Rigidbody>();
+        InvokeRepeating("InvokeMove", 3f, randRepeat);
+        SetRotation();
+        SetSpeed();
+    }
+
+    
+    void Update()
+    {
+    }
+
+    void SetSpeed(int speed = 1)
+    {
+        randRepeat = Random.Range(1, 10);
+        randSpeed = Random.Range(20, 70);
+
+        rg.velocity = new Vector3(0,0,0);
+        rg.AddForce(transform.rotation * Vector3.left * randSpeed * speed);
+    }
+
+    void SetRotation()
+    {
+        randRotY = Random.Range(0, 360);
+        transform.Rotate(new Vector3(0, randRotY, 0));        
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        // Debug.Log("Col!!" + this.gameObject.name);
+        // SetRotation();
+        // SetSpeed();
+    }
+
+    private void InvokeMove()
+    {
+        if (bAggro) return;
+
+        SetRotation();
+        SetSpeed();
+    }
+
+    public void SetAggro(GameObject Rod)
+    {
+        Transform tr = this.transform;
+        bAggro = true;
+        SetSpeed(0);
+
+        Vector3 Direction = Rod.transform.position - tr.position;
+        tr.rotation = Quaternion.Slerp(
+            tr.rotation,
+            Quaternion.LookRotation(Direction),
+            1f
+        );
+
+        tr.rotation = Quaternion.Euler(
+            new Vector3(tr.eulerAngles.x, tr.eulerAngles.y + 90, tr.eulerAngles.z));
+    }
+
+    public void ResetAggro()
+    {
+        bAggro = false;
+    }
+}
---
+++ Script/Rod.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rod : MonoBehaviour
+{
+    Fish targetFish;
+    bool bAggro;
+    float AggroTime;
+    Queue<Collider> QCol;
+    void Start()
+    {
+        QCol = new Queue<Collider>();
+        bAggro = false;
+        AggroTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WaitUntilAggro();
+        StartRodding();
+    }
+
+    private void WaitUntilAggro()
+    {
+        if(QCol.Count > 0)
+        {
+            targetFish = QCol.Peek().gameObject.GetComponent<Fish>();
+            if(targetFish != null)
+            {
+                AggroTime += Time.deltaTime;
+            }
+
+            if (bAggro == false & AggroTime > 2f && targetFish != null)
+            {
+                Debug.Log("Aggro get ---- " + targetFish.gameObject.name);        
+                targetFish.SetAggro(this.gameObject);
+                bAggro = true;
+            }
+        }
+    }
+
+    private void StartRodding()
+    {
+        if(!bAggro) { return; }
+
+        if(AggroTime > 4f)
+        {
+            Debug.Log("Rod Finish ---- " + targetFish.gameObject.name);
+            FinishRodding();
+        }
+    }
+
+    private void FinishRodding()
+    {
+        Destroy(targetFish.gameObject);
+        targetFish = null;
+        AggroTime = 0;
+        bAggro = false;
+        QCol.Dequeue();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // 이러면 하나만 Queue에 들어오게되니 수정해야함
+        if (targetFish) { return;}
+
+        Debug.Log("Trigger Enter!!!! ---- " + other.gameObject.name);        
+
+        if(!QCol.Contains(other)) {
+            QCol.Enqueue(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(QCol.Contains(other)) 
+        {
+            Debug.Log("Trigger Exit  " + other.gameObject.name);        
+            QCol.Dequeue();
+
+            if(targetFish != null) 
+            {
+                targetFish.ResetAggro();
+                bAggro = false;
+                targetFish = null;
+            }
+        }
+    }
+}
