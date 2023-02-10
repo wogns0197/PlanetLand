bdcc3622da49fcff38cbfd2c8fb9cdc046b42c60
KwonJaehoon <wogns0197@gmail.com>
2023-02-11 01:51:27 +0900

    보트 움직임 로직 변경

--- Script/Object/Ship.cs
+++ Script/Object/Ship.cs
@@ -19,17 +19,38 @@ public class Ship : MonoBehaviour
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        transform.position += new Vector3(-x, 0, -y) * speed * 2 * Time.deltaTime;
-        
-        
-        if(x == 1) {
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 0 )), speed * Time.deltaTime); 
+
+        if (y != 0)
+        {
+            this.transform.Translate(new Vector3(0.1f * -y,0,0));
         }
+
+        if (x != 0)
+        {
+            Quaternion q = this.transform.rotation;
+            this.transform.rotation = Quaternion.Slerp(q, Quaternion.Euler(q.eulerAngles + new Vector3(0, 0, x*20 )), speed * Time.deltaTime);
+        }
+
+        if (rg.velocity.x > 8 ){
+            rg.velocity = new Vector3(8, 0);
+        }
+
+        else if (rg.velocity.x < -8 ){
+            rg.velocity = new Vector3(-8, 0);
+        }
+
+        // transform.position += new Vector3(-x, 0, -y) * speed * 2 * Time.deltaTime;
+    
+
+        
+        // if(x == 1) {
+        //     this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 0 )), speed * Time.deltaTime); 
+        // }
             
-        else if(x == -1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 180 )), speed * Time.deltaTime);
-        if(y == 1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 270 )), speed * Time.deltaTime);
-        else if(y == -1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 90 )), speed * Time.deltaTime);
-        // rg.rotation = Quaternion.Slerp(this.rg.rotation, toRot, speed / 2 * Time.deltaTime);
+        // else if(x == -1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 180 )), speed * Time.deltaTime);
+        // if(y == 1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 270 )), speed * Time.deltaTime);
+        // else if(y == -1) this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(new Vector3(-90, 0, 90 )), speed * Time.deltaTime);
+
        
     }
 }
