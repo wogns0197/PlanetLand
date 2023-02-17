dae4d2b51323c2830b6d9f484b324770f011c0b7
KwonJaehoon <wogns0197@gmail.com>
2023-02-17 15:34:11 +0900

    인벤토리 페이징 추가, 컨텐츠 프리팹으로 전환

--- Script/UI/Inventory/UIInventory.cs
+++ Script/UI/Inventory/UIInventory.cs
@@ -15,7 +15,9 @@ enum ButtonType
 public class UIInventory : UIBase
 {
     public Button TypeBut0, TypeBut1, TypeBut2, TypeBut3, TypeBut4;
-    public TextMeshPro Tmpro0, Tmpro1, Tmpro2, Tmpro3, Tmpro4;
+    public Button ContentsLBut, ContentsRBut;
+    public TextMeshProUGUI Tmpro0, Tmpro1, Tmpro2, Tmpro3, Tmpro4;
+    public GameObject ContentBox;
 
     private string UnSelectColor = "#E9E9E9";
     private string SelectColor = "#4795FF";
@@ -28,19 +30,30 @@ public class UIInventory : UIBase
         TypeBut2.onClick.AddListener(OnClickedBut2);
         TypeBut3.onClick.AddListener(OnClickedBut3);
         TypeBut4.onClick.AddListener(OnClickedBut4);
-        Tmpro0 = TypeBut0.transform.GetChild(0).GetComponent<TextMeshPro>();
-        Tmpro1 = TypeBut1.transform.GetChild(0).GetComponent<TextMeshPro>();
-        Tmpro2 = TypeBut2.transform.GetChild(0).GetComponent<TextMeshPro>();
-        Tmpro3 = TypeBut3.transform.GetChild(0).GetComponent<TextMeshPro>();
-        Tmpro4 = TypeBut4.transform.GetChild(0).GetComponent<TextMeshPro>();
+        Tmpro0 = TypeBut0.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Tmpro1 = TypeBut1.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Tmpro2 = TypeBut2.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Tmpro3 = TypeBut3.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Tmpro4 = TypeBut4.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
 
-        if(Tmpro0.color == null)
-            Debug.Log("12");
+        InitInventory();
 
     }
     void Update()
     {
         
+    }
+
+    void InitInventory()
+    {
+        // 여기서 캐릭터 인벤토리 data기준으로 Usable, NotUsable 설정(태그포함) 해주고
+        // Usable 설정 이후의 프리팹 프리뷰로 넣어주기
+        // 일단 지금은 Usable 4개, Not Usable 12개로 set 해둠
+
+        for (int i = 0; i < ContentBox.transform.childCount; i++)
+        {
+            
+        }
     }
 
     void ResetButtonFocus()
@@ -55,7 +68,7 @@ public class UIInventory : UIBase
         TypeBut4.image.color = color;
 
         Color colorT;
-        ColorUtility.TryParseHtmlString("#ffffff", out colorT);
+        ColorUtility.TryParseHtmlString("#323232", out colorT);
 
 
         Tmpro0.color = colorT;
@@ -71,7 +84,7 @@ public class UIInventory : UIBase
         ColorUtility.TryParseHtmlString(SelectColor, out color);
 
         Color colorT;
-        ColorUtility.TryParseHtmlString("#000000", out colorT);
+        ColorUtility.TryParseHtmlString("#ffffff", out colorT);
 
         switch (i)
         {
